cf11f71fb3898d9ccdc7b785f5ec024857f83cf9
Nikko <88990996+neex18@users.noreply.github.com>
2021-09-28 21:50:34 -0700

    initial Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+
+// Prompt 
+// Password length between 8-128 characters.
+// True or False: Include; lowercase, uppercase, numeric, and/or special character
+
+var lowEnd = 8;
+var highEnd = 128;
+var arr = [];
+while(lowEnd <= highEnd){
+ arr.push(lowEnd++);
+}
+console.log(arr);
+
+var upCase = true
+var lowCase = true
+var numeric = true
+var specCharc = true
